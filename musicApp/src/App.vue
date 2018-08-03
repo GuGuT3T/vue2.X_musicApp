432b563290a2e0a5ed504ffc87d0ac5f6c91e5ea
guyuankun <634220187@qq.com>
2018-08-03 17:38:22 +0800

    框架搭建

--- musicApp/src/App.vue
+++ musicApp/src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
   </div>
 </template>
-
 <script>
+import mHeader from 'components/m-header/m-header.vue'
 export default {
-  name: 'App'
+  components: {
+    mHeader
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- musicApp/src/main.js
+++ musicApp/src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'babal-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastclick from 'fastclick' // 解决app点击事件延迟300毫秒
+import '../src/common/stylus/index.styl'
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
